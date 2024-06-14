4fbbf1ebfb0b18f74bc5cad16359cb7cbc56a213
SpecialNoel <jianminglin2893@gmail.com>
2024-06-14 19:48:42 -0400

    Updated comments in MultiConnEchoServer.py Updated comments in MultiConnEchoClient.py

--- Echo/MultiConnEchoClient.py
+++ Echo/MultiConnEchoClient.py
@@ -1,44 +1,43 @@
 # MultiConnEchoClient.py
 
 # Example commands to run this program: python3 MultiConnEchoClient.py 127.0.0.1 65432 2
+# *Note: The server side program (MultiConnEchoServer.py) has to run first
 
 import sys
 import socket
 import selectors
 import types
 
-# Start establishing connections from num_conns number of client sockets to the server socket
+# Start establishing connections to the server socket for num_conns number of client sockets
 # num_conns is read from the command-line and is the number of connections to create to the server.
 def start_connections(host, port, num_conns, selector, messages):
     # Server socket IP address and port number initialized
     server_addr = (host, port)
+
     # Do the following process for each client socket
     for i in range(0, num_conns):
         # Give this client socket an id for identification
         connid = i + 1
-        print(f"\nStarting connection {connid} to {server_addr}")
+        print(f"\nClient establishes connection {connid} to {server_addr}")
+
         # socket.AF_INET means that it has a host with an IPv4 address, and a port number which is an integer
         # socket.SOCK_STREAM means that the protocol used is TCP (Transmission Control Protocol)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         # setblocking(False) makes this client socket no longer block
         sock.setblocking(False)
+
         # Connect this client socket with the server socket using .connect_ex()
         # We use .connect_ex() here instead of .connect() because .connect() would immediately raise a BlockingIOError exception 
         #   (since .connect() should be used under a blocking environment). 
         # The .connect_ex() method initially returns an error indicator, errno.EINPROGRESS, 
         #   instead of raising an exception that would interfere with the connection in progress
         sock.connect_ex(server_addr)
+        
         # events can either be selectors.EVENT_READ or selectors.EVENT_WRITE since the socket is ready for reading and writing
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
+
         # More information on types.SimpleNamespace can be found in MultiConnEchoServer.py
-        # data is the object that holds the data info we want to be included along with the socket
-        # The messages that the client will send to the server are copied using messages.copy() 
-        #   because each connection will call socket.send() and modify the list. 
-        # Everything needed to keep track of 
-        #   (1) what the client needs to send, 
-        #   (2) has sent, and 
-        #   (3) has received, including the total number of bytes in the messages, 
-        #   is stored in the object data.
         data = types.SimpleNamespace(
             connid=connid,
             msg_total=sum(len(m) for m in messages),
@@ -46,50 +45,52 @@ def start_connections(host, port, num_conns, selector, messages):
             messages=messages.copy(),
             outb=b'',
         )
+
         # .register() registers this client socket to be monitored with selector.select()
         # For this client socket, it would want read events: selectors.EVENT_READ or selectors.EVENT_WRITE
         # data will store what's being sent and received on this client socket
         selector.register(sock, events, data=data)
 
-# Serve the connection
-# key is a SelectorKey namedtuple returned from selector.select() that 
-#   contains the socket object (fileobj) and data object
+# Perform services on the connection between the client socket and the server socket
+# key contains the socket object (fileobj) and data object
 # mask contains the events that are ready
+# selector is registered with the client socket and events: selectors.EVENT_READ OR selectors.EVENT_WRITE
 def serve(key, mask, selector):
     socket = key.fileobj
-    # data will not be None upon entering this function
     data = key.data
+
     # Handle reading event if the socket is ready for reading
     if mask & selectors.EVENT_READ:
-        recv_data = socket.recv(1024)  # Should be ready to read from the server socket
+        recv_data = socket.recv(1024)
+
         # If there are data received
         if recv_data:
             # Client side keeps track of the number of bytes it received from the server so that it can close its side of the connection
             # When the server detects this, it closes its side of the connection too
-            print(f"\nReceived {recv_data!r} from connection {data.connid}")
             data.recv_total += len(recv_data)
+            print(f"\nClient receives message: {recv_data!r} from connection {data.connid}")
+
         # If there are no data received, it means that the server side wants to close the connection with this client socket
-        # *Notice that the server now depends on the client being well-behaved: 
-        #   the server expects the client to close its side of the connection when it’s done sending messages. 
-        # *If the client doesn’t close, the server will leave the connection open.
-        # *In a real application, we may want to guard against this in server by implementing a timeout event
-        #   to prevent client connections from accumulating if they don’t send a request after a certain amount of time.
         if not recv_data or (data.recv_total == data.msg_total):
-            print(f"\nClosing connection to {data.connid}")
-            # Unsubscribe this client socket from selector.select()
+            # Remove this client socket from selector.select()
             selector.unregister(socket)
-            # After unregistering (unsubscribing), close this client socket (thus the connection with the server socket is fully closed)
+
+            # After unregistering, close this client socket (thus the connection with the server socket is fully closed)
             socket.close()
+            print(f"\nClient closes connection to {data.connid}")
+
     # Handle writing event if the socket is ready for writing
     if mask & selectors.EVENT_WRITE:
         if not data.outb and data.messages:
             data.outb = data.messages.pop(0)
+            
         if data.outb:
-            print(f"\nSending {data.outb!r} to connection {data.connid}")
-            # Any received data stored in data.outb is echoed to the server socket using sock.send()
+            # Any received data stored in data.outb is sent to the server socket using sock.send()
             # The .send() method returns the number of bytes sent. 
             # This number can then be used with slice notation on the .outb buffer to discard the bytes sent.
             sent = socket.send(data.outb)  # Should be ready to write to the server socket
+            print(f"\nClient sends message: {data.outb!r} to connection {data.connid}")
+
             # After sending the stored data to the server socket, remove the sent bytes from the send buffer
             data.outb = data.outb[sent:]
 
@@ -100,12 +101,14 @@ def start_serving_connection(selector):
             # selector.select(timeout=1) blocks until there are sockets ready for I/O
             # It returns a list of tuples, one for each socket, with each tuple containing a key and a mask
             events = selector.select(timeout=1)
+
             if events:
                 # key is a SelectorKey namedtuple that contains a fileobj attribute, with key.fileobject being a socket object
                 # mask is an event mask of the operations that are ready
                 for key, mask in events:
                     # Need to serve the connection by calling .serve()
                     serve(key, mask, selector)
+
             # Check for a socket being monitored to continue.
             if not selector.get_map():
                 break
@@ -117,13 +120,14 @@ def start_serving_connection(selector):
 if __name__ == '__main__':
     # This ensures that this program will be ran with appropriate commands accompanied
     if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <host> <port> <num_connections>")
+        print(f"Wrong command format. Usage: {sys.argv[0]} <host> <port> <num_connections>")
         sys.exit(1)
 
     # Client IP address and port number will be parsed from inputed commands
     host, port, num_conns = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
     # Two binary messages used for testing in client's side program
-    messages = [b"Message 1 from client.", b"Message 2 from client."]
+    messages = [b"Message 1, sent by client. ", 
+                b"Message 2, sent by client. "]
 
     # Introduce selector to handle multiple connection simultaneously
     selector = selectors.DefaultSelector()
--- Echo/MultiConnEchoServer.py
+++ Echo/MultiConnEchoServer.py
@@ -7,112 +7,128 @@ import socket
 import selectors
 import types
 
-# Accept the connection
-def accept(socket, selector):
-    # Accept connection established by a client socket
-    conn, address = socket.accept()  # Should be ready to read since the listening socket was registered with selectors.EVENT_READ
-    print(f"\nAccepted connection from {address}")
+# Accept the connection established by a client socket
+# socket is serverSocket
+# selector is registered with serverSocket and events: selectors.EVENT_READ
+def accept(serverSocket, selector):
+    # Should be ready to read since the listening socket was registered with selectors.EVENT_READ
     # conn is a new socket object (a sub socket created by the serverSocket)
     #   which can be used to send and receive data, to or from the connected client socket
-    # setblocking(False) to this sub socket will set it to non-blocking state
-    # If it blocks, then the entire server is stalled until it returns. 
-    # That means other sockets are left waiting even though the server isn’t actively working.
+    # For each client socket accepted, there will be one corresponding conn to be connected with it
+    conn, address = serverSocket.accept()
+    print(f"\nServer accepts connection from {address}")
+
+    # setblocking(False) to conn will set it to non-blocking state
+    # Reason to do so: if conn blocks, then the entire server is stalled until it returns. 
+    # This means other sockets are left waiting even though the server isn’t actively working
     conn.setblocking(False)
 
     # Credit for SimpleNamespace explanation: https://shorturl.at/Uo7B7
-    # types.SimpleNamespace is similar to an empty class, except that it has several advantages:
-    #   1. it allows you to initialize attributes while constructing the object
-    #   2. it provides a readable .repr()
-    #   3. it overrides the default comparison. Instead of comparing by .id(), it compares attribute values instead
+    # .SimpleNamespace is similar to an empty class, except that it has several advantages
     # data is the object that holds the data info we want to be included along with the socket
+    # Therefore, for every newly connected (accepted) client socket, there will be a corresponding data object
+    # each data is initialized with the client socket's address, an empty binary input variable and an empty binary output variable
     data = types.SimpleNamespace(addr=address, inb=b"", outb=b"")
-    # events can either be selectors.EVENT_READ or selectors.EVENT_WRITE since we want to know when the client connection is ready
-    #   for readinig and writing
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    # .register() registers this sub socket to be monitored with selector.select()
-    # For this sub socket, it would want read events: selectors.EVENT_READ or selectors.EVENT_WRITE
-    # data will store what's being sent and received on this sub socket
-    selector.register(conn, events, data=data)
 
-# Serve the client socket
-# key is a SelectorKey namedtuple returned from selector.select() that 
-#   contains the socket object (fileobj) and data object
+    # events should be selectors.EVENT_READ OR selectors.EVENT_WRITE 
+    #   since we want to know when the client connection is ready for readinig and writing
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    
+    # .register() registers conn to be monitored with selector.select()
+    # conn would want these events: selectors.EVENT_READ OR selectors.EVENT_WRITE
+    # data will store what's being sent and received by conn
+    selector.register(conn, events, data)
+
+# Perform services on the connection between conn and the client socket
+# key contains the socket object (fileobj) and data object
 # mask contains the events that are ready
+# selector is registered with conn and events: selectors.EVENT_READ OR selectors.EVENT_WRITE
 def serve(key, mask, selector):
+    # Retrive socket object and data object from key
     socket = key.fileobj
-    # data will not be None upon entering this function
     data = key.data
+
     # Handle reading event if the socket is ready for reading
     if mask & selectors.EVENT_READ:
-        recv_data = socket.recv(1024)  # Should be ready to read from the client socket
-        # If there are data received
+        recv_data = socket.recv(1024)
+        # If receiving data sent by client socket
         if recv_data:
-            # Append received data to data.outb to later sent it back to the client socket
+            # Append received data to data.outb to later sent it back to the client socket (echo)
             data.outb += recv_data
         else: 
-            # If there are no data received, it means that the client side wants to close the connection with this sub socket
-            print(f"\nClosing connection to {data.addr}")
+            # If there are no data received, it means that the client socket wants to close the connection with conn
             selector.unregister(socket)
             socket.close()
+            print(f"\nServer closes connection to {data.addr}")
+
     # Handle writing event if the socket is ready for writing
     if mask & selectors.EVENT_WRITE:
+        # Received data stored in data.outb, if any, is echoed to the client using sock.send()
         if data.outb:
-            print(f"\nEchoing {data.outb!r} to {data.addr}")
-            # Any received data stored in data.outb is echoed to the client using sock.send()
             # The .send() method returns the number of bytes sent. 
             # This number can then be used with slice notation on the .outb buffer to discard the bytes sent.
-            sent = socket.send(data.outb)  # Should be ready to write to the client socket
-            # After sending the stored data to the client socket, remove the sent bytes from the send buffer
+            sent = socket.send(data.outb)
+            print(f"\nServer echos message: {data.outb!r} to {data.addr}")
+            
+            # After sending the stored data to the client socket, clear the buffer by removing sent bytes
             data.outb = data.outb[sent:]
 
+# Setup the server socket
 def setup_serverSocket():
     # This ensures that this program will be ran with appropriate commands accompanied
     if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <host> <port>")
+        print(f"Wrong command format. Usage: {sys.argv[0]} <host> <port>")
         sys.exit(1)
 
     # Server IP address and port number will be parsed from inputed commands
     host, port = sys.argv[1], int(sys.argv[2])
+
+    # Create the server socket, call it serverSocket
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.bind((host, port))
+
+    # Make serverSocket start listening to connections (not yet accept any, just open for connection)
     serverSocket.listen()
-    print(f"Listening on {(host, port)}")
-    # setblocking(False) makes serverSocket no longer block
-    # When used with selector.select(), it will wait for events on one or more sockets,
+    print(f"Server listens on {(host, port)}")
+
+    # When setblocking(False) is used with selector.select(), it will wait for events on one or more sockets,
     #   and then read and write data when it's ready
     serverSocket.setblocking(False)
     return serverSocket
 
+# Setup the selector for serverSocket
 def setup_selector(serverSocket):
     # Introduce selector to handle multiple connection simultaneously
     selector = selectors.DefaultSelector()
+
     # .register() registers serverSocket to be monitored with selector.select()
-    # For the listening socket, it would want read events: selectors.EVENT_READ
+    # serverSocket would want read events: selectors.EVENT_READ
     # data will store what's being sent and received on serverSocket
     selector.register(serverSocket, selectors.EVENT_READ, data=None)
     return selector
 
+# Accepting or Serving connections from client sockets
+# selector is registered with serverSocket and events: selectors.EVENT_READ
 def start_listen_connections(selector):
     try:
-        # The server side program should keep running until an user keyboard input interrupt this program
+        # Keep running until an user keyboard input interrupt this program
         while True:
-            # selector.select(timeout=None) blocks until there are sockets ready for I/O
+            # selector.select(timeout=None) blocks indefinitely until there are sockets ready for reading or writing events
             # It returns a list of tuples, one for each socket, with each tuple containing a key and a mask
             events = selector.select(timeout=None)
-            # key is a SelectorKey namedtuple that contains a fileobj attribute, with key.fileobject being a socket object
-            # mask is an event mask of the operations that are ready
+
             for key, mask in events:
-                # If key.data is equal to None, then it is from the listening socket (see selector.register() above)
-                # Need to accept the connection by calling .accept()
+                # If key.data is equal to None, it means that key.fileobj is serverSocket (see setup_selector())
+                # We need to accept the connection by calling .accept()
                 if key.data is None:
                     accept(key.fileobj, selector)
                 else:
-                    # If key.data is not None, then it is a client socket that is already been accepted
-                    # Need to serve the client socket by calling .serve()
+                    # If key.data is not None, it means that key.fileobj is a client socket that has already been accepted
+                    # We need to serve the client socket by calling .serve()
                     serve(key, mask, selector)
     except KeyboardInterrupt:
         # Encountering user input [ctrl+c], thus breaking from the infinite loop of serving client sockets
-        print("Caught keyboard interrupt, exiting")
+        print("Caught user keyboard interrupt, exiting")
     finally:
         # Upon encountering user keyboard interrupt, closes the selector and exits the program
         selector.close()
